c1eb8281a41c135c15b291b9ef3ed9c80ea68e31
Damonkcey <medchouiref@outlook.com>
2023-06-03 19:09:30 +0200

    First successful print :)

---
+++ src/graphicsEngine/index.ts
@@ -0,0 +1,89 @@
+import ChessEngine from '../chessEngine';
+import { getRowColFromPos } from '../chessEngine/utils/commonFuncs.ts';
+import { AssetsMapper } from './utils/assetsMapper.ts';
+import { PieceCodeName } from '../chessEngine/entitites/pieces.entities.ts';
+
+class GraphicsEngine {
+	private static Instance: GraphicsEngine;
+	private chessEngineInstance: ChessEngine;
+	private canvasRef: HTMLCanvasElement;
+	private canvasCtx: CanvasRenderingContext2D;
+
+	private constructor() {
+		this.chessEngineInstance = ChessEngine.getInstance();
+		this.canvasRef = document.querySelector('#GAME_CANVAS') as HTMLCanvasElement;
+		this.canvasCtx = this.canvasRef.getContext('2d') as CanvasRenderingContext2D;
+
+		this.loadAssets()
+			.then(() => {
+				this.prepareGraphics();
+			})
+			.catch(() => {
+				debugger;
+			});
+	}
+
+	public static getInstance(): GraphicsEngine {
+		if (!this.Instance) {
+			this.Instance = new GraphicsEngine();
+		}
+		return this.Instance;
+	}
+
+	private loadAssets() {
+		return new Promise((resolve, reject) => {
+			const pieces = Object.keys(AssetsMapper.PiecesImages) as Array<PieceCodeName>;
+			let index: number = 0;
+			const loadImage = (piece: PieceCodeName) => {
+				index++;
+				const img = new Image();
+				img.src = `src/assets/pieces/${piece}.svg`;
+				img.onload = () => {
+					AssetsMapper.PiecesImages[piece] = img;
+					if (pieces[index]) {
+						loadImage(pieces[index]);
+					} else {
+						resolve(true);
+					}
+				};
+				img.onerror = () => {
+					reject(false);
+				};
+			};
+			loadImage(pieces[index]);
+		});
+	}
+
+	public prepareGraphics(): void {
+		this.canvasRef.width = this.canvasRef.clientWidth * window.devicePixelRatio;
+		this.canvasRef.height = this.canvasRef.clientHeight * window.devicePixelRatio;
+
+		this.gameLoop();
+	}
+
+	private gameLoop(): void {
+		window.requestAnimationFrame(() => this.gameLoop());
+
+		this.canvasCtx.clearRect(0, 0, this.canvasRef.width, this.canvasRef.height);
+
+		this.chessEngineInstance.getBoard().forEach((_row) => {
+			_row.forEach((_col) => {
+				const { row, col } = getRowColFromPos(_col.position);
+				const squareWidth = this.canvasRef.width / 8;
+				this.canvasCtx.fillStyle = (row + col) % 2 === 0 ? '#fff' : '#e3bda2';
+				this.canvasCtx.fillRect(squareWidth * col, squareWidth * row, squareWidth, squareWidth);
+				if (_col.piece) {
+					this.canvasCtx.drawImage(
+						AssetsMapper.PiecesImages[_col.piece.codeName] as HTMLImageElement,
+						squareWidth * col,
+						squareWidth * row,
+						squareWidth,
+						squareWidth,
+					);
+				}
+			});
+		});
+	}
+}
+
+export default GraphicsEngine;
---
+++ src/graphicsEngine/utils/assetsMapper.ts
@@ -0,0 +1,18 @@
+import { AssetsMapperType } from '../entities/assetsMapper.entities.ts';
+
+export const AssetsMapper: AssetsMapperType = {
+	PiecesImages: {
+		B_P: undefined,
+		W_P: undefined,
+		B_B: undefined,
+		W_B: undefined,
+		B_Kn: undefined,
+		W_Kn: undefined,
+		B_R: undefined,
+		W_R: undefined,
+		B_Q: undefined,
+		W_Q: undefined,
+		B_K: undefined,
+		W_K: undefined,
+	},
+};
